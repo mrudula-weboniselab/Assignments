40b8c10631beb8a6788bfec953656b933e5b8d59
mrudula-weboniselab <mrudula.patil@weboniselab.com>
2015-04-01 15:22:33 +0530

    changes main file

--- event-handling/src/main/java/org/mrudula/MainEventHandlerApp.java
+++ event-handling/src/main/java/org/mrudula/MainEventHandlerApp.java
@@ -1,25 +1,29 @@
 package org.mrudula;
-
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.mrudula.events.mouse.ClickToZoom;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 /**
  * Created by webonise on 31-03-2015.
  */
 public class MainEventHandlerApp extends Application {
     private static final Logger LOG = LoggerFactory.getLogger(MainEventHandlerApp.class);
-
-    //public static void main(String[] args) { launch(args); }
-
+    public static void main(String[] args) { launch(args); }
     @Override
     public void start(final Stage stage) throws Exception {
-        LOG.info("Starting ....");
-        ApplicationContext context = new AnnotationConfigApplicationContext(MainEventHandlerAppConfiguration.class);
-        MainEventController mainEventController = context.getBean( MainEventController.class);
-        mainEventController.launch(stage);
+        stage.setTitle("Left click to zoom in, right click to zoom out");
+        ImageView imageView = new ImageView("/images/images.jpg");
+        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(150);
+        imageView.setOnMouseClicked(new ClickToZoom());
+        final StackPane layout = new StackPane();
+        layout.getChildren().addAll(imageView);
+        layout.setStyle("-fx-background-color: LightCyan;");
+        stage.setScene(new Scene(layout, 400, 500));
+        stage.show();
     }
 }
